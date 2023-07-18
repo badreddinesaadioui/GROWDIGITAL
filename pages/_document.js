ce864d33fbe3202abbf9a6f28d70c1c7de83afcf
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-07-18 16:02:01 +0100

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,15 @@
-import config from "@config/config.json";
-import { Head, Html, Main, NextScript } from "next/document";
+import Head from "next/head";
+import { Html, Main, NextScript } from "next/document";
+import favicon from './banana.svg';
 
 const Document = () => {
-  // destructuring items from config object
-  const { favicon } = config.site;
   return (
     <Html lang="en">
       <Head>
         {/* favicon */}
         <link
           rel="shortcut icon"
-          type="image/png"
-          sizes="32x32"
+          type="image/svg+xml"  // Update the type to "image/svg+xml" for SVG favicon
           href={favicon}
         />
         {/* theme meta */}
