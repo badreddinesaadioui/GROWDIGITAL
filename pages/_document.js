e41ef1efa08c38a970056f2001134df5d1fa7bd6
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-07-18 16:03:40 +0100

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,38 +1,38 @@
-import Head from "next/head";
-import { Html, Main, NextScript } from "next/document";
-import favicon from './banana.svg';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const Document = () => {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* favicon */}
-        <link
-          rel="shortcut icon"
-          type="image/svg+xml"  // Update the type to "image/svg+xml" for SVG favicon
-          href={favicon}
-        />
-        {/* theme meta */}
-        <meta name="theme-name" content="next-boilerplate" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: light)"
-          content="#fff"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#000"
-        />
-      </Head>
-      <body>
-        <Main />
-        {/* <TwSizeIndicator /> */}
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* favicon */}
+          <link
+            rel="shortcut icon"
+            type="image/svg+xml" // Update the type to "image/svg+xml" for SVG favicon
+            href="/banana.svg" // Make sure to provide the correct path to the banana.svg file
+          />
+          {/* theme meta */}
+          <meta name="theme-name" content="next-boilerplate" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#fff"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#000"
+          />
+        </Head>
+        <body>
+          <Main />
+          {/* <TwSizeIndicator /> */}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default Document;
+export default MyDocument;
