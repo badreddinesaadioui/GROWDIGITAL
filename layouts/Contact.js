b611b9280c788deccf70bf842355dce981cbcaf6
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-07-20 14:13:48 +0100

    Contact.js

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,46 +1,10 @@
 import config from "@config/config.json";
 import { markdownify } from "@lib/utils/textConverter";
-import Email from "https://smtpjs.com/v3/smtp.js"; // Import the SMTP.js library
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title, info } = frontmatter;
   const { contact_form_action } = config.params;
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    // Get form data
-    const formData = new FormData(event.target);
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const subject = formData.get("subject");
-    const message = formData.get("message");
-
-    // Prepare the email content with input elements' values
-    const emailContent = `
-      Name: ${name}
-      Email: ${email}
-      Subject: ${subject}
-      Message: ${message}
-    `;
-
-    // Send email using SMTP.js
-    Email.send({
-      Host: "smtp.elasticemail.com", // SMTP server host (replace with your server)
-      Username: "your_smtp_username", // Your SMTP username
-      Password: "your_smtp_password", // Your SMTP password or API key
-      To: "recipient@example.com", // Recipient's email address (replace with the actual recipient)
-      From: email, // Sender's email address (using the entered email as the "From" address)
-      Subject: "Contact Form Submission", // Email subject
-      Body: emailContent, // Email body
-    }).then((message) => {
-      alert("Email sent successfully!"); // Show a success message
-    }).catch((error) => {
-      alert("Failed to send email. Please try again later."); // Show an error message
-      console.error(error);
-    });
-  };
 
   return (
     <section className="section">
@@ -52,7 +16,6 @@ const Contact = ({ data }) => {
               className="contact-form"
               method="POST"
               action={contact_form_action}
-              onSubmit={handleFormSubmit} // Call the handleFormSubmit function on form submission
             >
               <div className="mb-3">
                 <input
@@ -85,7 +48,6 @@ const Contact = ({ data }) => {
                 <textarea
                   className="form-textarea w-full rounded-md"
                   rows="7"
-                  name="message" // Added the "name" attribute for the textarea
                   placeholder="Your message"
                 />
               </div>
